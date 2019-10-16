0d75ec367544742e270e01d5d10325a4be29de0d
luhuifang <645903151@qq.com>
2019-10-16 09:12:04 +0800

    修改更改IP后的地址断言

--- test_case/D003_network_test.py
+++ test_case/D003_network_test.py
@@ -51,7 +51,7 @@ class ModifyingNetworkTest2(MyTest):
             login = LoginPage(self.driver)
             login.login_sys(readconfig.username, readconfig.password)
             sleep(2)
-            self.assertEqual(new_url,"%slogin.html" % readconfig.url)
+            self.assertEqual(new_url,"%s/login.html" % readconfig.url)
             self.assertEqual(self.driver.title, u"录播管理系统")
         except Exception as msg:
             logger.error(u"异常原因：%s"%msg)
