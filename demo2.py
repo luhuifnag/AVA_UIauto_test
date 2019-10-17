4c63f35274d95ab8393af462bbde2692bdaf5b32
luhuifang <645903151@qq.com>
2019-10-17 15:20:57 +0800

    124录制页面测试

--- demo2.py
+++ demo2.py
@@ -5,36 +5,20 @@ Created on 2019年04月16日
 @author: Aloe
 '''
 
-import time
-import unittest
-from time import sleep
+try:
+        try:
+            "1"=="2"
+            # print('199')
+        except:
+        print("111")
+        print("111")
+        print("111")
+        print("111")
+        print("111")
+        print("111")
+        print("111")
+        print("111")
+except:
+    print('0000')
 
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as ES
-from selenium.webdriver.support.ui import WebDriverWait
-
-from models import readconfig
-from pages.home_page import HomePage
-from pages.loginpage import LoginPage
-from utils.log import logger
-
-class MyTest(unittest.TestCase):
-
-    def setUp(self):
-        logger.info(u"******************测试开始******************")
-        self.driver = webdriver.Firefox()
-        sleep(4)
-        self.driver.maximize_window()
-        self.driver.get("http://192.168.13.120/")
-        self.driver.implicitly_wait(20)
-        login = LoginPage(self.driver)
-        login.login_sys(readconfig.username, readconfig.password)
-        sleep(2)
-        WebDriverWait(self.driver,5,0.5).until(ES.title_is(u"录播管理系统")) 
-    
-
-            
-if __name__ == "__main__":
-
-    unittest.main()
         
